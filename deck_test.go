f872a3d89be9a8d1d3b8720d1608573cf6509132
Avijeet Pandey <avijeetpandey25@gmail.com>
2023-04-08 22:50:25 +0530

    Add: Test for saving deck to file sytem

--- deck_test.go
+++ deck_test.go
@@ -1,11 +1,25 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	cards := newDeck()
 
 	if len(cards) != 16 {
 		t.Errorf("Expected deck length of 16, but got %v", len(cards))
+	}
+}
+
+func TestSavingToFile(t *testing.T) {
+	cards := newDeck()
+
+	cards.saveToFile("my_cards")
+
+	if _, err := os.Stat("./my_cards"); errors.Is(err, os.ErrNotExist) {
+		t.Errorf("Failed saving deck to the file system")
 	}
 }
